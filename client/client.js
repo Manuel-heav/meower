87c5dbfe04dc6a5ca5b3bbb2ff6234ad5e2626fc
manuel-heav <manuelheav32@gmail.com>
2022-06-10 20:49:43 +0300

    working on frontend

--- client/client.js
+++ client/client.js
@@ -1,10 +1,22 @@
 console.log("hello world");
 
 const form = document.querySelector("form");
+const loadingElement = document.querySelector(".loading");
+
+loadingElement.style.display = "none";
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const formData = new FormData(form);
   const name = formData.get("name");
   const content = formData.get("content");
+
+  const mew = {
+    name,
+    content,
+  };
+
+  console.log(mew);
+  form.style.display = "none";
+  loadingElement.style.display = "";
 });
