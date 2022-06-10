a61e4b087d4d33a4c75ef182ce8590a01859b356
manuel-heav <manuelheav32@gmail.com>
2022-06-10 20:40:27 +0300

    working on frontend

--- client/client.js
+++ client/client.js
@@ -4,5 +4,7 @@ const form = document.querySelector("form");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log("form submitted");
+  const formData = new FormData(form);
+  const name = formData.get("name");
+  const content = formData.get("content");
 });
