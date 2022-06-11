c03b50e09718d70bad059d3eec44398b2472e913
manuel-heav <manuelheav32@gmail.com>
2022-06-11 18:11:52 +0300

    backend started

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(morgan());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "damn",
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
